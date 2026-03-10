5d3709dc50e4e3047f972ea945491c03efaa3d18
rafaolegario <rafaelbisofficel@gmail.com>
2026-03-10 10:35:30 -0300

    fix: remove hash make on password

--- database/seeders/admin_user_seeder.ts
+++ database/seeders/admin_user_seeder.ts
@@ -1,6 +1,5 @@
 import { Roles } from '#enums/roles'
 import User from '#models/user'
-import Hash from '@ioc:Adonis/Core/Hash'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
 export default class AdminUserSeeder extends BaseSeeder {
@@ -9,7 +8,7 @@ export default class AdminUserSeeder extends BaseSeeder {
     if (!exists) {
       await User.create({
         email: 'admin@betalent.com',
-        password: await Hash.make('admin123'),
+        password: 'admin123',
         role: Roles.ADMIN,
       })
     }
