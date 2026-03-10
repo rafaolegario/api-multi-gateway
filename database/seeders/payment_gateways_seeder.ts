4f120930f26fb0abe938319dbffb245e70c02b50
rafaolegario <rafaelbisofficel@gmail.com>
2026-03-10 19:54:52 -0300

    feat: create payment gateways seed

---
+++ database/seeders/payment_gateways_seeder.ts
@@ -0,0 +1,22 @@
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import Gateway from '#models/gateway'
+
+export default class PaymentGatewaysSeeder extends BaseSeeder {
+  public async run() {
+    const exists = await Gateway.query().whereIn('name', ['gateway1', 'gateway2'])
+    if (!exists) {
+      await Gateway.createMany([
+        {
+          name: 'gateway1',
+          isActive: true,
+          priority: 1,
+        },
+        {
+          name: 'gateway2',
+          isActive: true,
+          priority: 2,
+        },
+      ])
+    }
+  }
+}
