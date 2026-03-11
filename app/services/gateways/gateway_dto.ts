c4320635494cf807db4b536ef9e2167c964b3592
rafaolegario <rafaelbisofficel@gmail.com>
2026-03-10 22:41:50 -0300

    feat: implement gateway dto and service

---
+++ app/services/gateways/gateway_dto.ts
@@ -0,0 +1,9 @@
+export interface GatewayToggleIsActiveDTO {
+  gatewayId: string
+  isActive: boolean
+}
+
+export interface GatewayChangePriorityDTO {
+  gatewayId: string
+  priority: number
+}
---
+++ app/services/gateways/gateway_service.ts
@@ -0,0 +1,49 @@
+import type Gateway from '#models/gateway'
+import { type GatewayRepository } from '#repositories/contracts/gateway_repository'
+import { type PaginatedResult, type PaginationParams } from '../../types/index.ts'
+import { type GatewayChangePriorityDTO, type GatewayToggleIsActiveDTO } from './gateway_dto.ts'
+
+export class GatewayService {
+  constructor(private gatewayRepository: GatewayRepository) { }
+
+  async listGateways(pagination: PaginationParams): Promise<PaginatedResult<Gateway>> {
+    return await this.gatewayRepository.findAll(pagination)
+  }
+
+  async toggleGatewayIsActive(data: GatewayToggleIsActiveDTO): Promise<Gateway> {
+    const { gatewayId, isActive } = data
+
+    const gateway = await this.gatewayRepository.findById(gatewayId)
+
+    if (!gateway) {
+      throw new Error(`Gateway with ID ${gatewayId} not found`)
+    }
+
+    if (gateway.isActive !== isActive) {
+      gateway.isActive = isActive
+      return await this.gatewayRepository.update(gateway)
+    }
+
+    return gateway
+  }
+
+  async changeGatewayPriority(data: GatewayChangePriorityDTO): Promise<Gateway> {
+    const { gatewayId, priority } = data
+
+    const gateway = await this.gatewayRepository.findById(gatewayId)
+
+    if (!gateway) {
+      throw new Error(`Gateway with ID ${gatewayId} not found`)
+    }
+
+    if (priority <= 0) {
+      throw new Error('Priority cannot be zero or negative')
+    }
+
+    if (gateway.priority !== priority) {
+      gateway.priority = priority
+      return await this.gatewayRepository.update(gateway)
+    }
+    return gateway
+  }
+}
