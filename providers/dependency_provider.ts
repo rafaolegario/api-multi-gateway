a59544b90c7856873604b1f501fccc8593f3ba6f
rafaolegario <rafaelbisofficel@gmail.com>
2026-03-11 18:03:19 -0300

    feat: add new dependences

--- providers/dependency_provider.ts
+++ providers/dependency_provider.ts
@@ -4,7 +4,18 @@ import { LucidUserRepository } from '#repositories/lucid/lucid_user_repository'
 import { TokenProvider } from '#services/auth/contracts/token_provider'
 import { DbTokenProvider } from '#services/auth/db_token_provider'
 import { AuthenticateService } from '#services/auth/authenticate_service'
+import { GatewayRepository } from '#repositories/contracts/gateway_repository'
+import { LucidGatewayRepository } from '#repositories/lucid/lucid_gateway_repository'
+import { TransactionRepository } from '#repositories/contracts/transaction_repository'
+import { LucidTransactionRepository } from '#repositories/lucid/lucid_transaction_repository'
+import { ClientRepository } from '#repositories/contracts/client_repository'
+import { LucidClientRepository } from '#repositories/lucid/lucid_client_repository'
+import { ProductRepository } from '#repositories/contracts/product_repository'
+import { LucidProductRepository } from '#repositories/lucid/lucid_product_repository'
+import { PurchaseService } from '#services/purchase/purchase_service'
+import { GatewayRegistry } from '../app/payment_gateways/gateway_registry.ts'
 
+//Para fins do teste tecnico injetei tudo aqui, mas o ideal seria criar um provider específico para cada serviço para manter a organização e escalabilidade do projeto.
 export default class DependencyProvider {
   constructor(protected app: ApplicationService) {}
 
@@ -15,6 +26,42 @@ export default class DependencyProvider {
 
     this.app.container.singleton(TokenProvider, () => {
       return new DbTokenProvider()
+    })
+
+    this.app.container.singleton(GatewayRepository, () => {
+      return new LucidGatewayRepository()
+    })
+
+    this.app.container.singleton(TransactionRepository, () => {
+      return new LucidTransactionRepository()
+    })
+
+    this.app.container.singleton(ClientRepository, () => {
+      return new LucidClientRepository()
+    })
+
+    this.app.container.singleton(ProductRepository, () => {
+      return new LucidProductRepository()
+    })
+
+    this.app.container.singleton(GatewayRegistry, () => {
+      return new GatewayRegistry()
+    })
+
+    this.app.container.singleton(PurchaseService, async () => {
+      const gatewayRepository = await this.app.container.make(GatewayRepository)
+      const gatewayRegistry = await this.app.container.make(GatewayRegistry)
+      const clientRepository = await this.app.container.make(ClientRepository)
+      const productRepository = await this.app.container.make(ProductRepository)
+      const transactionRepository = await this.app.container.make(TransactionRepository)
+
+      return new PurchaseService(
+        gatewayRepository,
+        gatewayRegistry,
+        clientRepository,
+        productRepository,
+        transactionRepository
+      )
     })
 
     this.app.container.singleton(AuthenticateService, async () => {
