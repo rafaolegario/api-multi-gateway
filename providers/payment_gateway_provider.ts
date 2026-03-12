30f273e8cea4e1af9e44859778e30758c53fe627
rafaolegario <rafaelbisofficel@gmail.com>
2026-03-12 10:42:33 -0300

    feat: registry gateway1

--- providers/payment_gateway_provider.ts
+++ providers/payment_gateway_provider.ts
@@ -2,6 +2,7 @@ import type { ApplicationService } from '@adonisjs/core/types'
 
 import { GatewayRegistry } from '../app/payment_gateways/gateway_registry.ts'
 import { Gateway2 } from '../app/payment_gateways/gateway_2.ts'
+import { Gateway1 } from '../app/payment_gateways/gateway_1.ts'
 
 export default class PaymentGatewayProvider {
   constructor(protected app: ApplicationService) {}
@@ -10,6 +11,7 @@ export default class PaymentGatewayProvider {
     this.app.container.singleton(GatewayRegistry, () => {
       const registry = new GatewayRegistry()
 
+      registry.register('gateway1', new Gateway1())
       registry.register('gateway2', new Gateway2())
 
       return registry
