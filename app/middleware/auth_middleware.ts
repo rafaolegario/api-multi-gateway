3b6e8b8493f71596ff4a8418a2e1c4004de280d0
rafaolegario <rafaelbisofficel@gmail.com>
2026-03-12 12:23:39 -0300

    fix: use corret method

--- app/middleware/auth_middleware.ts
+++ app/middleware/auth_middleware.ts
@@ -2,8 +2,12 @@ import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
 
 export default class AuthenticationMiddleware {
-  async handle({ auth }: HttpContext, next: NextFn) {
-    auth.getUserOrFail()
-    await next()
+  async handle({ auth, response }: HttpContext, next: NextFn) {
+    try {
+      await auth.authenticate()
+      await next()
+    } catch (error) {
+      return response.unauthorized({ message: 'Not authenticated' })
+    }
   }
 }
