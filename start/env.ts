4dbff932000d7ae0cf03c43840f921055abedbbc
rafaolegario <rafaelbisofficel@gmail.com>
2026-03-13 18:19:37 -0300

    feat: add env

--- start/env.ts
+++ start/env.ts
@@ -2,6 +2,7 @@ import { Env } from '@adonisjs/core/env'
 
 export default await Env.create(new URL('../', import.meta.url), {
   // Node
+  NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
   PORT: Env.schema.number(),
   HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.string(),
