02cc4e62b142a3628673abfafff3233288546414
rafaolegario <rafaelbisofficel@gmail.com>
2026-03-13 10:05:38 -0300

    fix: remove unused params

--- app/validators/transactions_validator.ts
+++ app/validators/transactions_validator.ts
@@ -4,11 +4,8 @@ import { TransactionStatus } from '#enums/transaction_status'
 export const listTransactionsQuery = vine.create({
   page: vine.number().min(1),
   limit: vine.number().min(1).max(100),
-  clientId: vine.string().uuid().optional(),
   gatewayId: vine.string().uuid().optional(),
   status: vine.enum(TransactionStatus).optional(),
-  startDate: vine.date().optional(),
-  endDate: vine.date().optional(),
 })
 
 export const getTransactionParams = vine.create({
